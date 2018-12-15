875fa4530ae8fd163eb4fb6fdcc82caa5cce6be2
Edward Newman <ebnewman@gmail.com>
2018-12-15 16:38:44 +0000

    Updated to use nodemon

--- test/create_test.js
+++ test/create_test.js
@@ -2,9 +2,14 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('save a user', () => {
+  it('save a user', (done) => {
     const joe = new User({ name: 'Joe' });
 
-    joe.save();
+    joe.save()
+      .then(() => {
+        // Has Joe been saved successfully?
+        assert(!joe.isNew);
+        done();
+      });
   });
 });
