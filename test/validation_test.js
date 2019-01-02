ba752bcab44d932863c48ddbddc650430de0ad8c
Edward Newman <ebnewman@gmail.com>
2019-01-02 14:27:04 +0000

    first validation test added

---
+++ test/validation_test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Validating records', () => {
+  it('requires a user name', () => {
+    const user = new User({
+      name: undefined,
+    });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name is required.');
+    console.log('name required validation worked');
+  });
+});
