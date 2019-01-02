5ef590354fe35a475a1557c4843aa6aeb5f6e292
Edward Newman <ebnewman@gmail.com>
2019-01-02 22:08:12 +0000

    added subdocument tests

--- src/user.js
+++ src/user.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
+const PostSchema = require('./post');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   name: {
     type: String,
+    validate: {
+      validator: (name) => name.length > 2,
+      message: 'Name must be longer than 2 characters.',
+    },
     required: [true, 'Name is required.'],
   },
   postCount: Number,
+  posts: [PostSchema],
 });
 
 const User = mongoose.model('user', UserSchema);
--- test/validation_test.js
+++ test/validation_test.js
@@ -12,4 +12,25 @@ describe('Validating records', () => {
     assert(message === 'Name is required.');
     console.log('name required validation worked');
   });
+
+  it('requires a user\'s name longer than 2 characters', () => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name must be longer than 2 characters.');
+    console.log('name length validation worked');
+  });
+
+  it('disallows invalid records from being saved', (done) => {
+    const user = new User({ name: 'Al' });
+    user.save()
+      .catch((validationResult) => {
+        const { message } = validationResult.errors.name;
+
+        assert(message === 'Name must be longer than 2 characters.');
+        console.log('invalid record disallowed worked');
+        done();
+      });
+  });
 });
