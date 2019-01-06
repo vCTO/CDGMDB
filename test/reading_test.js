a203005ca4b8310cbd3f3c350b72a5fae2f144da
Edward Newman <ebnewman@gmail.com>
2019-01-06 12:27:52 +0000

    added skip and limit test

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,11 +2,15 @@ const assert = require('assert');
 const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
-  let joe;
+  let joe, maria, alex, zach;
 
   beforeEach((done) => {
+    alex = new User({ name: 'Alex' });
     joe = new User({ name: 'Joe' });
-    joe.save()
+    maria = new User({ name: 'Maria' });
+    zach = new User({ name: 'Zach' });
+
+    Promise.all([alex.save(), joe.save(), maria.save(), zach.save() ])
       .then(() => done());
   });
 
@@ -28,4 +32,17 @@ describe('Reading users out of the database', () => {
       });
   });
 
+  it('can skip and limit the result set', (done) => {
+    User.find({})
+      .sort({ name: 1 })
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users.length === 2);
+        assert(users[0].name === 'Joe');
+        assert(users[1].name === 'Maria');
+      });
+    console.log('skip and limit worked');
+    done();
+  });
 });
